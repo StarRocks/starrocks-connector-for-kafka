5c1ba1b6369936896d6ed8e512c4e719bc88db21
Zaorang Yang <zaorangy@gmail.com>
2023-12-13 17:16:11 +0800

    [Feature] Suppport decimal type parse
    Signed-off-by: Zaorang Yang <zaorangy@gmail.com>

--- src/test/java/com/starrocks/connector/kafka/json/JsonConverterTest.java
+++ src/test/java/com/starrocks/connector/kafka/json/JsonConverterTest.java
@@ -3,14 +3,22 @@ package com.starrocks.connector.kafka.json;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.starrocks.connector.kafka.StarRocksSinkTask;
 import org.apache.kafka.connect.data.SchemaAndValue;
+import org.apache.log4j.PropertyConfigurator;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class JsonConverterTest {
+    @Before
+    public void setUp() {
+        PropertyConfigurator.configure("src/test/conf/log4j.properties");
+    }
+
     private SchemaAndValue getSchemaAndValueFromJsonStr(String jsonStr) throws JsonProcessingException {
         JsonConverter jsonConverter = new JsonConverter();
         Map<String, Object> props = new HashMap<>();
@@ -25,10 +33,10 @@ public class JsonConverterTest {
     }
 
     @Test
-    public void convertToJson() throws JsonProcessingException {
+    public void testConvertToJson() throws JsonProcessingException {
         String jsonStr = "{\"elements\":[{\"elName\":\"zll\",\"age\":1},{\"elName\":\"zll1\",\"age\":2}],\"name\":\"haha\",\"id\":1}";
         SchemaAndValue schemaAndValue = getSchemaAndValueFromJsonStr(jsonStr);
-        JsonConverter jsonConverter = new JsonConverter();
+        JsonConverter jsonConverter = StarRocksSinkTask.createJsonConverter();
         JsonNode jsonNodeDest = jsonConverter.convertToJson(schemaAndValue.schema(), schemaAndValue.value());
         System.out.println(jsonNodeDest.toString());
         Assert.assertEquals(jsonStr, jsonNodeDest.toString());
